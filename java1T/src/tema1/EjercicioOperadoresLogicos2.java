aa722b61be0cf965d29c29c99fddc1d390989edd
Mohamed <simo2026kh@gmail.com>
2025-09-30 16:33:16 +0200

    302/09/2025 update EjemploOperadoresLogicos2

---
+++ java1T/src/tema1/EjercicioOperadoresLogicos2.java
@@ -0,0 +1,28 @@
+package tema1;
+
+public class EjercicioOperadoresLogicos2 {
+
+    public static void main(String[] args) {
+        //Un cliente recibe un descuento especial si se cumplen estas condiciones:
+        //Es miembro VIP y su compra supera 100€ O
+        //No es VIP pero es su cumpleaños y la compra es mayor a 50€
+        //En ningún caso hay descuento si el cliente tiene pagos pendientes
+
+
+        boolean clienteVip = true;
+        double valorCompra = 135;
+        boolean esCumpleaños = false;
+        boolean tienePagosPendientes = false;
+        boolean resultado;
+
+        //Dar valor a las variables de entrada
+        //Poner la condición
+        //Ver lo que debe dar y comprobar el resultado pintándolo
+
+
+        resultado = (!tienePagosPendientes) && ((clienteVip && valorCompra > 100) || (!clienteVip && esCumpleaños && valorCompra > 50));
+
+        System.out.println(resultado);
+    }
+}
+
