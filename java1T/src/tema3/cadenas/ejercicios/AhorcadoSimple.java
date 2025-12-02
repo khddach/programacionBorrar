80ae7cb0ec077247bc78749ef0353bf581b2ddb4
Mohamed <simo2026kh@gmail.com>
2025-12-02 18:16:08 +0100

    Merge remote-tracking branch 'origin/main'

---
+++ java1T/src/tema3/cadenas/ejercicios/AhorcadoSimple.java
@@ -0,0 +1,75 @@
+package tema3.cadenas.ejercicios;
+
+import java.util.Scanner;
+
+public class AhorcadoSimple {
+    public static void main(String[] args) {
+        /**
+         * Implementa el juego del ahorcado con una palabra predefinida (ej: "JAVA").
+         * Muestra al inicio una serie de guiones (_) del tamaño de la palabra.
+         * El jugador ingresa letras de una en una.
+         * Si la letra está en la palabra, revela su posición (o posiciones) reemplazando el guión.
+         * Si no, resta una vida (comienza con 6 vidas).
+         * Usa un array de char para la palabra oculta y otro para la palabra con pistas.
+         * Gana si adivina la palabra antes de quedarse sin vidas.
+         */
+
+
+
+
+        String palabra = "amiga";
+        Scanner sc = new Scanner(System.in);
+        char letra;
+        int contador = 0;
+        int intentos = 6;
+
+
+        String palabraX = "_".repeat(palabra.length()) ;
+        StringBuilder sb = new StringBuilder(palabraX);
+
+
+        while (true) {
+
+            if (intentos == 0) {
+                System.out.println("juego terminado");
+            }
+
+            if (contador == palabra.length()) {
+                System.out.println("gana");
+                break;
+            }
+
+            System.out.println("escribe una palabra :");
+            letra = sc.nextLine().charAt(0);
+
+
+
+            boolean esChar = false;
+
+            for (int i = 0; i < palabra.length(); i++) {
+
+                if (palabra.charAt(i) == letra && sb.charAt(i) != letra) {
+                    sb.setCharAt(i,letra);
+                    contador++;
+                    esChar = true;
+                }
+
+            }
+
+
+           if (!esChar) {
+               intentos--;
+               System.out.println("intentos es " + intentos);
+           }
+
+
+           System.out.println("palabra es " + sb.toString());
+
+
+
+        }
+
+
+
+    }
+}
